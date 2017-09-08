aa6f06b77848a756b8013cf06de802b2583ccc3d
kamdong <kamdong@DESKTOP-6RM4PTH>
2017-09-08 13:56:31 +0900

    hello world

---
+++ 201623504-JavaBeanJDBC/src/testBean/studentVO.java
@@ -0,0 +1,21 @@
+package testBean;
+
+public class studentVO {
+	private String number,name;
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String numeber) {
+		this.number = numeber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
